1635fc5adf60f124e47ef3b4d4177083e6d11299
Carl Wanninger <carlwanninger@gmx.de>
2025-10-16 13:38:41 +0200

    single_sim_facelift

--- grecco_sim/scripts/simulation/single_sim.py
+++ grecco_sim/scripts/simulation/single_sim.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pathlib
 import datetime
 import pytz
@@ -6,37 +8,37 @@ import matplotlib.pyplot as plt
 from grecco_sim.simulator import simulation_setup
 from grecco_sim.util import type_defs, logger
 
+year = 2033
+data_root = pathlib.Path(__file__).parent(4) / "data"
+
 OPFINGEN = {
     "name": "opfingen_2033_c",
     "n_agents": 4,
     # Create symlink into data directory!
-    "grid_data_path": pathlib.Path(__file__).parent.parent.parent.parent
-    / "data"
-    / "2033"
-    / "2033_evconservative",
-    "weather_data_path": pathlib.Path(__file__).parent.parent.parent.parent
-    / "data"
-    / "2033"
-    / "weather_data.csv",
-    "ev_capacity_data_path": pathlib.Path(__file__).parent.parent.parent.parent
-    / "data"
-    / "synpro_ev_data_pool.csv",  # if file not available, capacities are set to 60kWh
-    "heat_demand_path": pathlib.Path(__file__).parent.parent.parent.parent
-    / "data"
-    / "2033"
-    / "2033_evconservative"
-    / "heat_demand.csv",
+    "grid_data_path": data_root / f"{year}" / "2033_evconservative",
+    "weather_data_path": data_root / f"{year}" / "weather_data.csv",
+    "ev_capacity_data_path": data_root / f"{year}" /
+                             "synpro_ev_data_pool.csv",
+    # if file not available, capacities are set to 60kWh
+    "heat_demand_path": data_root / f"{year}" / "2033_evconservative" /
+                        "heat_demand.csv",
     "hp": True,
     "ev": True,
     "bat": True,
 }
 
 
-def main(coord_type: str, start_time: datetime.datetime, days: int = 7, name: str = ""):
+def main(
+        coord_type: str,
+        start_time: datetime.datetime,
+        days: int = 7,
+        sim_name: Optional[str] = None):
 
     date = start_time.date()
-    if not name:
-        name = f"{coord_type}_{date}"
+
+    if sim_name is None:
+        sim_name = f"{coord_type}_{date}"
+
     run_parameters = simulation_setup.RunParameters(
         sim_horizon=days * 24 * 4,
         # start_time=datetime.datetime(2024, 1, 1, 0, 0, 0, tzinfo=pytz.utc),
@@ -51,7 +53,7 @@ def main(coord_type: str, start_time: datetime.datetime, days: int = 7, name: st
         plot=True,
         show=False,
         profile_run=True,
-        output_file_dir=pathlib.Path("default") / name,
+        output_file_dir=pathlib.Path("default") / sim_name,
     )
 
     opt_pars = type_defs.OptParameters(
@@ -77,4 +79,4 @@ if __name__ == "__main__":
     start = datetime.datetime(2020, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
     days = 10
 
-    main(coord, start, days, name="none_2033_hp_test")
+    main(coord, start, days, sim_name="none_2033_hp_test")
